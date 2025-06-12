e60eb9f8af25126f83a101232ea099838bd9f6fc
Jake Smith <jakethegreatest93@gmail.com>
2025-06-12 12:57:03 +0100

    Add subtraction functionality to runGame and calculateCorrectAnswer

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,7 +33,10 @@ function runGame(gameType) {
         displayAdditionQuestion(num1, num2);
     } else if (gameType === "multiply") {
         displayMultiplyQuestion(num1, num2);
-    } else {
+    } else if (gameType === "subtract") {
+        displaySubtractQuestion(num1, num2);
+    }
+    else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
@@ -77,7 +80,7 @@ function calculateCorrectAnswer() {
     } else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
-        return [operand1 - operand2, "subtraction"];
+        return [operand1 - operand2, "subtract"];
     }
     
     else {
@@ -111,10 +114,10 @@ function displayAdditionQuestion(operand1, operand2) {
     
 }
 
-function displaySubtractQuestion() {
+function displaySubtractQuestion(operand1, operand2) {
     
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById('operator').textContent = "-";
 }
 
